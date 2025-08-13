6d53db46ffe86d66ae851e5460003ca4416b7c86
Bianca Lucas <biaxx.iluks@gmail.com>
2025-08-13 13:15:40 -0300

    feat: add user unit test

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -49,4 +49,55 @@ describe("UsersService", () => {
         expect(result).toEqual(users);
         expect(mockPrisma.user.findMany).toHaveBeenCalled();
     })
+
+    // -> 3. Teste de Busca de Usuário por ID
+    it("deve encontrar um usuário pelo id", async () => {
+        const user = { id: '550e8400-e29b-41d4-a716-446655440000', name: "Bianca Lucas", email: "bia05@gmail.com" };
+        mockPrisma.user.findUnique.mockResolvedValue(user);
+        const result = await service.findOne('550e8400-e29b-41d4-a716-446655440000');
+        expect(result).toEqual(user);
+        expect(mockPrisma.user.findUnique).toHaveBeenCalledWith({
+            where: { id: '550e8400-e29b-41d4-a716-446655440000' },
+        });
+    })
+
+    // -> 4. Teste de Exceção para Usuário Não Encontrado
+    it("deve lançar uma exceção se o usuário não for encontrado", async () => {
+        mockPrisma.user.findUnique.mockResolvedValue(null);
+        await expect(service.findOne('550e8400-e29b-41d4-a716-4466554404848848')).rejects.toThrow(NotFoundException);
+        expect(mockPrisma.user.findUnique).toHaveBeenCalledWith({
+            where: { id: '550e8400-e29b-41d4-a716-4466554404848848' },
+        });
+    })
+
+    // -> 5. Teste de Atualização de Usuário
+    it("deve atualizar um usuário", async () => {
+        const dto = { name: "Bianca Lucas", email: "bia05@gmail.com" };
+        const updatedUser = { ...dto, id: '550e8400-e29b-41d4-a716-446655440000' };
+        mockPrisma.user.update.mockResolvedValue(updatedUser);
+        const result = await service.UpdateUser('550e8400-e29b-41d4-a716-446655440000', dto);
+        expect(result).toEqual(updatedUser);
+        expect(mockPrisma.user.update).toHaveBeenCalledWith({
+            where: { id: '550e8400-e29b-41d4-a716-446655440000' },
+            data: dto,
+        });
+    })
+
+    // -> 6. Teste de Remoção de Usuário
+    it("deve remover um usuário", async () => {
+        const user = {
+            id: '550e8400-e29b-41d4-a716-446655440000',
+            name: "Bianca Lucas",
+            email: "bia05@gmail.com"
+        };
+
+        mockPrisma.user.delete.mockResolvedValue(user);
+
+        const result = await service.remove('550e8400-e29b-41d4-a716-446655440000');
+
+        expect(result).toEqual(user);
+        expect(mockPrisma.user.delete).toHaveBeenCalledWith({
+            where: { id: '550e8400-e29b-41d4-a716-446655440000' },
+        });
+    });
 })
